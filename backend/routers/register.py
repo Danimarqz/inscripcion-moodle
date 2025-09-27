009d4e148b85c0e394ccfc00294ecd170029deab
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2025-09-27 22:55:53 +0200

    feat: enhance registration process by adding error handling for Moodle user creation and updating email notifications

--- backend/routers/register.py
+++ backend/routers/register.py
@@ -21,26 +21,34 @@ async def register(request: Request, data: RegisterData):
     check_rate_limit(request)
     if hasattr(data, "website") and data.website:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Spam detectado")
-
+    error_creando_moodle = False
     try:
         form_data = data.model_dump()
         pdf_bytes = generate_pdf(form_data)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al generar el PDF: {str(e)}")
-
-    try:
-        send_emails(data.email, pdf_bytes, data.name, data.surname)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al enviar el correo: {str(e)}")
-
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error al generar el PDF: {str(e)}"
+        )
     try:
         create_moodle_user(form_data)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al crear usuario en Moodle: {str(e)}")
+        print(f"Error al crear usuario en Moodle: {str(e)}")
+        error_creando_moodle = True
+
+    try:
+        send_emails(data.email, pdf_bytes, data.name, data.surname, error_creando_moodle)
+    except Exception as e:
+        print(f"Error en envio de emails: {str(e)}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error al enviar el correo: {str(e)}")
 
     try:
         post_registration_to_gsheet(form_data)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al registrar en Google Sheets: {str(e)}")
+        print(f"Error en envio a gsheet: {str(e)}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error al registrar en Google Sheets: {str(e)}")
 
+    if error_creando_moodle:
+        print("se envia el mensaje modificado")
+        return {"message": "Tu inscripción se gestionará lo antes posible"}
     return {"message": "Inscripción completada correctamente"}
--- backend/services/register/email_utils.py
+++ backend/services/register/email_utils.py
@@ -2,9 +2,18 @@ import smtplib
 from email.message import EmailMessage
 from config import SMTP_USER, SMTP_PASS, SMTP_SERVER, SMTP_PORT, ADMIN_EMAIL
 
-def send_emails(user_email: str, pdf_bytes: bytes, user_name: str, user_surname: str) -> None:
+def send_emails(user_email: str, pdf_bytes: bytes, user_name: str, user_surname: str, error_creando_moodle: bool) -> None:
     subject = "Confirmación de inscripción"
-    body = f"""
+    error_moodle = f"\nEl email {user_email} ya estaba usado en moodle. Por favor, revísalo.\n"
+    body_admin = f"""
+    Hola {user_name} {user_surname},
+    {error_moodle if error_creando_moodle else ""}
+    Gracias por completar tu inscripción. Adjuntamos el PDF generado con tus datos.
+
+    Un saludo,
+    Equipo de OpositaTCAE
+    """
+    body_user = f"""
     Hola {user_name} {user_surname},
 
     Gracias por completar tu inscripción. Adjuntamos el PDF generado con tus datos.
@@ -12,9 +21,8 @@ def send_emails(user_email: str, pdf_bytes: bytes, user_name: str, user_surname:
     Un saludo,
     Equipo de OpositaTCAE
     """
-
     # Crear mensaje base
-    def build_msg(to_email: str) -> EmailMessage:
+    def build_msg(to_email: str, body: str) -> EmailMessage:
         msg = EmailMessage()
         msg["Subject"] = subject
         msg["From"] = SMTP_USER
@@ -28,8 +36,8 @@ def send_emails(user_email: str, pdf_bytes: bytes, user_name: str, user_surname:
         )
         return msg
 
-    msg_user = build_msg(user_email)
-    msg_admin = build_msg(ADMIN_EMAIL)
+    msg_user = build_msg(user_email, body_user)
+    msg_admin = build_msg(ADMIN_EMAIL, body_admin)
 
     with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as smtp:
         smtp.starttls()
